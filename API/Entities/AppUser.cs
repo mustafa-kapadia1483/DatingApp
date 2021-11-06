92686e8851ea05da1810d8b5207ea02c5d0fcfc2
mustafa-kapadia1483 <mustafakapadia76@gmail.com>
2021-11-07 00:24:33 +0530

    API basic structure created

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public   int Id { get; set; }
+        public String UserName { get; set; }
+
+        // Full Implementation of above short hand implementation
+        /* private int myVar;
+         public int MyProperty
+         {
+             get { return myVar; }
+             set { myVar = value; }
+         }*/
+        
+    }
+}
