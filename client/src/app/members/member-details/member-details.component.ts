95960d537f9d37869effc679e302b3397a3e840e
mustafa-kapadia1483 <mustafakapadia76@gmail.com>
2021-11-09 16:52:23 +0530

    added ui for member cards, added jwt interceptor in angular

--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-details',
   templateUrl: './member-details.component.html',
-  styleUrls: ['./member-details.component.css']
+  styleUrls: ['./member-details.component.css'],
 })
 export class MemberDetailsComponent implements OnInit {
+  member: Member;
 
-  constructor() { }
+  constructor(
+    private membersService: MembersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    this.membersService
+      .getMember(this.route.snapshot.paramMap.get('username'))
+      .subscribe((member) => (this.member = member));
+  }
 }
